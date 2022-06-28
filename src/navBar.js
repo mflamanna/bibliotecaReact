b631d8e868e1e8be02c673151012088de8a6614b
mflamanna <lamanna.mflorencia@gmail.com>
2022-06-28 13:54:14 +0200

    changes

---
+++ src/navBar.js
@@ -0,0 +1,17 @@
+
+
+function NavBar (){
+    return (
+       <>
+        <nav className=".navbar-brand">
+          <ul>
+            <li>Home</li>
+            <li>Create</li>
+            <li>Registration</li>
+          </ul>
+        </nav>
+       </>
+    )
+}
+
+export default NavBar;
