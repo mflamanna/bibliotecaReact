551eb10a434076947b1b3abe64fb73027479511d
unknown <you@example.com>
2022-06-29 12:46:06 +0200

    boton de pagina Welcome funcionando

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,5 +1,6 @@
 import React from "react";
-import '../styles/welcome.css'
+import { NavLink } from "react-router-dom";
+import '../styles/welcome.css';
 import welcoImage from "../assets/img/welcome-image.jpg";
 
 function Welcome(){
@@ -8,7 +9,10 @@ function Welcome(){
     <div className = "welcome-container">
         <div className = "welcome-sec-left">
             <h2 className = "welcome-h2" >Welcome to Feminist Library</h2>
-            <button type = "button" className="btn btn-secondary">Press here to start</button>   
+            <NavLink to ="/principal">
+              <button type = "button" className="btn btn-secondary" >Press here to start</button>   
+            </NavLink>
+              
         </div>
         <div className = "welcome-sec-right">
             <img className="welcome-img" src = {welcoImage} alt = "Imagen libro"/>  
