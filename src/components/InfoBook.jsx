1bf8e847aed6716c335933984aad3eef484ee1e7
Nika <nikaguisasola@gmail.com>
2022-07-01 09:14:19 +0200

    actualizado

--- src/components/InfoBook.jsx
+++ src/components/InfoBook.jsx
@@ -6,13 +6,13 @@ function InfoBook () {
         <div class="card mb-3">
         <div class="row g-0">
             <div class="col-md-4">
-            <img src="{image}" class="img-fluid rounded-start" alt="..."></img>
+            <img src="https://images-na.ssl-images-amazon.com/images/I/819SKuVK7bL.jpg" class="img-fluid rounded-start" alt="Feminismo para principiantes"></img>
             </div>
             <div className="col-md-8">
                 <div className="card-body">
-                <h5 className="card-title">Nombre del libro</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Read more</small></p>
+                <h5 className="card-title">Feminismo para principiantes</h5>
+                <p className="card-text">¿Quienes eran las sufragistas? ¿De dónde sale el feminismo radical? ¿Por que se habla de marxismo y feminismo como de un matrimonio mal avenido? ¿Por que el feminismo ha sido vilipendiado y ridiculizado? ¿Por que las feministas han sido tratadas de "marimachos", feas o mujeres sexualmente insatisfechas? ¿Cómo y dónde surge la expresión "violencia de genero"? ¿En que consiste la masculinidad tradicional? A partir de estos interrogantes, y otros muchos, Nuria Varela repasa tres siglos de hacer y deshacer el mundo, de alumbrar teorías, propuestas y liderazgos fascinantes, y narra la aventura de una agitación social que ningún otro movimiento ha conseguido mantener durante tanto tiempo.</p>
+                <p className="card-text"><small className="text-muted">Autora: Nuria Varela</small></p>
                 </div>
             </div>
             </div>  
