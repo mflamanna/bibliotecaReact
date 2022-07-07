de4c740081a294aa389e36b6e091ae6459fe3cdd
mflamanna <lamanna.mflorencia@gmail.com>
2022-07-07 11:33:16 +0200

    image not found

--- src/components/ImageBook.jsx
+++ src/components/ImageBook.jsx
@@ -7,10 +7,23 @@ function ImageBook ({isbn}){
         .then (response=>response.json())
         .then (data=>setImgBook(data))
     }, [])
-    
-    return(
-        <img className="cardImgBook card-img-top" src={"https://openlibrary.org/isbn/"+isbn+".json"} alt ="imagen de libro"/> 
-    )
+    if (typeof imgBook === 'object'){
+        if (imgBook.covers === undefined || imgBook.covers === null){
+            return(
+                <img className="cardImgBook card-img-top" src={"https://www.forewordreviews.com/books/covers/bunk-9s-guide-to-growing-up.jpg"} alt ="imagen de libro"/> 
+            )
+        } else {
+            return(
+                <img className="cardImgBook card-img-top" src={`https://covers.openlibrary.org/b/id/${imgBook.covers}.jpg`} alt ="imagen de libro"/> 
+            )
+
+        }
+    } else {
+        return(
+            <img className="cardImgBook card-img-top" src={"https://www.forewordreviews.com/books/covers/bunk-9s-guide-to-growing-up.jpg"} alt ="imagen de libro"/> 
+        )
+    }
+   
 }
 
 export default ImageBook;
